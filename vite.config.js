fbb908a1a6588ce8fa7e5c2f4ce24ee3137b6047
DmitryLasuta <lasutadima01@gmail.com>
2023-09-06 03:10:45 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+import sass from 'sass'
+import htmlMinifier from 'vite-plugin-html-minifier'
+
+export default defineConfig({
+	css: {
+		preprocessorOptions: {
+			scss: {
+				implementation: sass,
+			},
+			postcss: {
+				plugins: [autoprefixer],
+			},
+		},
+	},
+
+	plugins: [htmlMinifier()],
+})
